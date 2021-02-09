b5a39657df43969cd7f41eec5113116c62d2a961
Ethiraj Krishnamanaidu <ekrishnamanaidu@slb.com>
2021-02-09 10:54:15 -0600

    Apache headers

--- app/bulk_persistence/__init__.py
+++ app/bulk_persistence/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Schlumberger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .bulk_id import BulkId
 from .dataframe_persistence import create_and_store_dataframe, get_dataframe
 from .dataframe_serializer import DataframeSerializer
--- app/injector/az_injector.py
+++ app/injector/az_injector.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Schlumberger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from osdu.core.api.storage.blob_storage_base import BlobStorageBase
 from osdu_az.storage.blob_storage_az import AzureAioBlobStorage
 from .app_injector import AppInjector, AppInjectorModule
