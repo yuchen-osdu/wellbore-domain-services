967dd78e954cfa16a8ff596fdf302b1dfbda84bf
Yunhua Koglin <kogliny@amazon.com>
2021-03-18 15:15:00 +0000

    remove duplication which could lead to outdated lib requirements
    commit 9eecba5e 
    Author: Yunhua Koglin <kogliny@amazon.com> 
    Date: Thu Mar 18 2021 08:04:17 GMT-0500 (Central Daylight Time) 
    
        update requirements.txt for test
    
    
    commit 2dbf6802 
    Author: Yunhua Koglin <kogliny@amazon.com> 
    Date: Wed Mar 17 2021 20:15:50 GMT-0500 (Central Daylight Time) 
    
        Merge branch 'dev' of codecommit://os-wellbore-domain-services into kogliny
    
    
    commit 21e78ab4 
    Author: Yunhua Koglin <kogliny@amazon.com> 
    Date: Wed Mar 17 2021 15:17:31 GMT-0500 (Central Daylight Time) 
    
        remove account info
    
    
    commit 89d3aca3 
    Author: Yunhua Koglin <kogliny@amazon.com> 
    Date: Wed Mar 17 2021 12:30:07 GMT-0500 (Central Daylight Time) 
    
        Merge branch 'master' of codecommit://os-wellbore-domain-services into kogliny
    
    
    commit 8cdbf66e 
    Author: Yunhua Koglin <kogliny@amazon.com> 
    Date: Wed Mar 17 2021 11:13:31 GMT-0500 (Central Daylight Time) 
    
        merge from master branch

--- tests/aws-test/build-aws/prepare-dist.sh
+++ tests/aws-test/build-aws/prepare-dist.sh
@@ -23,6 +23,12 @@ INTEGRATION_TEST_OUTPUT_DIR=${INTEGRATION_TEST_OUTPUT_DIR:-$OUTPUT_DIR}/testing
 
 rm -rf "$INTEGRATION_TEST_OUTPUT_DIR"
 mkdir -p "$INTEGRATION_TEST_OUTPUT_DIR"
+
+if [ ! -e requirements_dev.txt ]; then
+    echo "File requirements_dev.txt does not exist!"
+else
+    cp requirements_dev.txt tests/aws-test/build-aws/requirements.txt
+fi
 cp  -r tests/aws-test "${INTEGRATION_TEST_OUTPUT_DIR}"
 cp  -r tests/integration "${INTEGRATION_TEST_OUTPUT_DIR}"
 cp  -r schema/indexation "${INTEGRATION_TEST_OUTPUT_DIR}"
