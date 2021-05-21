8090a416f87d7c60e81d509465df254498bd6dc4
Victor Dosda <vdosda@slb.com>
2021-05-21 16:48:45 +0200

    tests on value zero in dips data

--- tests/integration/functional/tests/test_dips.py
+++ tests/integration/functional/tests/test_dips.py
@@ -310,7 +310,7 @@ def test_insert_dips(get_env):
             },
             "azimuth": {
                 "unitKey": "dega",
-                "value": 10
+                "value": 0
             },
             "inclination": {
                 "unitKey": "dega",
