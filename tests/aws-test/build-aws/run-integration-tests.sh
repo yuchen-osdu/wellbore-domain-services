43cf71289703c47b2c99e8b61d7276f06e24a8a4
Yunhua Koglin <kogliny@amazon.com>
2021-05-27 14:10:06 +0000

    Update run-integration-tests.sh

--- tests/aws-test/build-aws/run-integration-tests.sh
+++ tests/aws-test/build-aws/run-integration-tests.sh
@@ -9,13 +9,14 @@ which yum && sudo yum install -y python3 python3-pip python3-venv || echo "Not R
 python3 -m venv env
 source env/bin/activate
 python3 -m pip install --upgrade pip
+python3 -m pip install -r frozenrequirements.txt
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements_dev.txt
 python3 -m pip install wheel pytest pytest-cov
 
 svc_url=$WELLBORE_DDMS_URL
 tenant='opendes'
-acl_domain='testing.com' 
+acl_domain='example.com'
 legal_tag='opendes-sdmstestlegaltag'
 svctoken=$(python tests/aws-test/build-aws/aws_jwt_client.py)
 
