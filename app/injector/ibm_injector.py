1c177f8e5fa1193e02e1a7c37e040702bb22f5a1
Yunhua Koglin <kogliny@amazon.com>
2021-08-20 10:47:13 -0500

    update

--- app/injector/ibm_injector.py
+++ app/injector/ibm_injector.py
@@ -1,33 +1,35 @@
-from osdu.core.api.storage.blob_storage_base import BlobStorageBase
-from osdu_ibm.storage.blob_storage_ibm import IBMObjectStorage
+# from osdu.core.api.storage.blob_storage_base import BlobStorageBase
+# from osdu_ibm.storage.blob_storage_ibm import IBMObjectStorage
 
-from app.utils import get_http_client_session
-from app.utils import Context
-from .app_injector import AppInjector, AppInjectorModule
-from app.bulk_persistence import resolve_tenant
-from app.bulk_persistence.dask.dask_bulk_storage import DaskBulkStorage
-from osdu_ibm.storage.dask_storage_parameters import get_dask_storage_parameters as ibm_parameters
+# from app.utils import get_http_client_session
+# from app.utils import Context
+# from .app_injector import AppInjector, AppInjectorModule
+# from app.bulk_persistence import resolve_tenant
+# from app.bulk_persistence.dask.dask_bulk_storage import DaskBulkStorage
+# from osdu_ibm.storage.dask_storage_parameters import get_dask_storage_parameters as ibm_parameters
 
 
 class IBMInjector(AppInjectorModule):
     def configure(self, app_injector: AppInjector):
-        app_injector.register(BlobStorageBase, IBMInjector.build_ibm_blob_storage)
-        app_injector.register(DaskBulkStorage, IBMInjector.build_ibm_dask_blob_storage)
+        raise NotImplementedError()
+        # app_injector.register(BlobStorageBase, IBMInjector.build_ibm_blob_storage)
+        # app_injector.register(DaskBulkStorage, IBMInjector.build_ibm_dask_blob_storage)
 
     @staticmethod
     async def build_ibm_blob_storage(*args, **kwargs) -> BlobStorageBase:
-        ctx: Context = Context.current()
-        # TODO to be reviewed
-        tenant = await resolve_tenant(ctx.partition_id)
-        return IBMObjectStorage(
-            session=get_http_client_session(),
-            service_account_file=tenant.credentials
-        )
+        raise NotImplementedError()
+        # ctx: Context = Context.current()
+        # # TODO to be reviewed
+        # tenant = await resolve_tenant(ctx.partition_id)
+        # return IBMObjectStorage(
+        #     session=get_http_client_session(),
+        #     service_account_file=tenant.credentials
+        # )
 
     @staticmethod
     async def build_ibm_dask_blob_storage() -> DaskBulkStorage:
-        # raise NotImplementedError()
-        ctx: Context = Context.current()
-        tenant = await resolve_tenant(ctx.partition_id)
-        params = await ibm_parameters(tenant)
-        return await DaskBulkStorage.create(params)
+        raise NotImplementedError()
+        # ctx: Context = Context.current()
+        # tenant = await resolve_tenant(ctx.partition_id)
+        # params = await ibm_parameters(tenant)
+        # return await DaskBulkStorage.create(params)
