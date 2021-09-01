4500dc11548733960a5c9ec8a1e69448102e21ea
DIR\FSerin <fserin@slb.com>
2021-09-01 09:58:18 +0200

    Reverting NOTICE and using anyOf instead oneOf

--- app/errors/validation_error.py
+++ app/errors/validation_error.py
@@ -36,11 +36,11 @@ async def http422_error_handler(
     get_logger().exception(f"http422_error_handler - {request.url}")
     return JSONResponse(content=jsonable_encoder({"errors": exc.errors()}), status_code=HTTP_422_UNPROCESSABLE_ENTITY)
 
-
+# TODO remove this once this fastapi issue is closed https://github.com/tiangolo/fastapi/issues/3790
 validation_error_definition["properties"] = {
     "loc": {
         "title": "Location", "type": "array", "items": {
-            "oneOf": [
+            "anyOf": [
                 {"type": "string"},
                 {"type": "integer"}
             ]
