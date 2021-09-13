25e83950475452fae5cde98c4cf21efad9659bc0
Victor Dosda <vdosda@slb.com>
2021-09-13 16:21:11 +0200

    Keep only generic loop to avoid if/else path.

--- app/routers/delete/delete_bulk_data.py
+++ app/routers/delete/delete_bulk_data.py
@@ -92,27 +92,15 @@ async def delete_purge_record(
         # Delete meta data
         await storage_client.purge_record(id=record_id, data_partition_id=ctx.partition_id)
 
-        # Get bulk_ids directly from storage to check if it's matching
-        # with the number of bulk uris retrieved from record version
-        bulk_ids = await dask_blob_storage.get_bulk_ids(record_id)
-
         tenant = await resolve_tenant(ctx.partition_id)
         storage: BlobStorageBase = await ctx.app_injector.get(BlobStorageBase)
         encode_record_id = dask_blob_storage.encode_record_id()
         bulk_file_names = await storage.list_objects(tenant=tenant,
                                                      prefix=encode_record_id)
-        # In tiny cases record_id sha1 can be similar with a other record_id sha1
-        # To delete only data relative to the record_id wanted, we deleting data by version instead of the entire folder
-        if len(record_bulk_uris) == len(bulk_ids):
-            await asyncio.gather(*[
-                storage.delete(tenant=tenant, object_name=bulk_file_name)
-                for bulk_file_name in bulk_file_names],
+        await asyncio.gather(*[
+            storage.delete(tenant=tenant, object_name=bulk_file_name)
+            for bulk_file_name in bulk_file_names
+            for bulk_id in record_bulk_uris if bulk_id in bulk_file_name],
                                  return_exceptions=True)
-        else:
-            await asyncio.gather(*[
-                storage.delete(tenant=tenant, object_name=bulk_file_name)
-                for bulk_file_name in bulk_file_names
-                for bulk_id in record_bulk_uris if bulk_id in bulk_file_name],
-                                     return_exceptions=True)
     else:
         await storage_client.delete_record(id=record_id, data_partition_id=ctx.partition_id)
