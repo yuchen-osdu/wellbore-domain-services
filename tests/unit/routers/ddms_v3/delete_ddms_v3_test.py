efab2642da83b1f69fea8f600ef00d7056e234e2
Victor Dosda <vdosda@slb.com>
2021-09-28 15:13:56 +0200

    Adapted tests

--- tests/unit/routers/ddms_v3/delete_ddms_v3_test.py
+++ tests/unit/routers/ddms_v3/delete_ddms_v3_test.py
@@ -120,6 +120,5 @@ def test_delete_purge_record(client_delete, logger_fixture):
         )
         assert response.status_code == status.HTTP_204_NO_CONTENT
         for i in range(4):
-            logger_exception = logger_fixture.exception.mock_calls[i].args[0].split(":")
-            assert logger_exception[0] == "Exception on bulk versions deletion"
-            assert logger_exception[1].count("Error 404 not found") == 1
+            logger_exception = logger_fixture.exception.mock_calls[i].args[0]
+            assert logger_exception == "Exception on bulk versions deletion: Error 404 not found"
