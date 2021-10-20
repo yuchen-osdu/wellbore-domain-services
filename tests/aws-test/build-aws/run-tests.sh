37f92216498904f65bfe70139a4cb2780e43895b
Yunhua Koglin <kogliny@amazon.com>
2021-10-20 10:21:09 -0500

    change int to basic

--- tests/aws-test/build-aws/run-tests.sh
+++ tests/aws-test/build-aws/run-tests.sh
@@ -52,7 +52,7 @@ legal_tag='opendes-sdmstestlegaltag'
 
 python3 gen_postman_env.py --token $token --base_url $svc_url --cloud_provider "aws" --acl_domain $acl_domain --legal_tag $legal_tag --data_partition $tenant
 
-pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=!search
+pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=basic
 
 TEST_EXIT_CODE=$?
 deactivate
