ccc73e12279b0e2dcc56f5badeba72e5c3ebd860
Christophe Lallement <clallement@slb.com>
2021-12-06 16:43:42 +0100

    improve documentation

--- app/consistency/unique.py
+++ app/consistency/unique.py
@@ -6,13 +6,24 @@ class DuplicatedIdError:
 
 
 def get_unique_ids(object_list: List[Any], attr_name: str) -> Tuple[Set[str], Optional[DuplicatedIdError]]:
+    """Check that the value of an attribute of all objects in a list are unique
+
+    Args:
+        object_list (List[Any]):  objects whose specified attribute must be unique
+        attr_name: the name of the attribute to check for each object of the list passed as argument
+
+    Returns:
+        The values of the attribute specified for each object passed in argument if they are all unique
+        otherwise an empty set and a DuplicatedIdError
+    """
+
     ids = set()
 
     # check all curve ids are unique
     if object_list:
-        # expression generator fetch attribut   and   evaluate on demand if attribut value is duplicated
+        # expression generator fetch attribute  and  evaluate on demand if attribute value is duplicated
         is_id_duplicated = (getattr(obj, attr_name) in ids or ids.add(getattr(obj, attr_name)) for obj in object_list)
         if any(is_id_duplicated):
-            return {}, DuplicatedIdError()
+            return set(), DuplicatedIdError()
 
     return ids, None
