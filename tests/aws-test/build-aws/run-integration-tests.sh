488b5b6e3909f856e31cec92e537eb3b50df8f31
Yunhua Koglin <kogliny@amazon.com>
2022-01-26 10:52:29 -0600

    reduce memory

--- tests/aws-test/build-aws/run-integration-tests.sh
+++ tests/aws-test/build-aws/run-integration-tests.sh
@@ -43,7 +43,7 @@ cd tests/integration
 
 python3 gen_postman_env.py --token $svctoken --base_url $svc_url --cloud_provider "aws" --acl_domain $acl_domain --legal_tag $legal_tag --data_partition $tenant
 
-pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=!search
+pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=basic
 
 echo Delete legaltag after Integration Tests...
 curl --location --request DELETE "$LEGAL_URL"'legaltags/opendes-wellddmstestlegaltag' \
