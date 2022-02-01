36b3cbd4e4940188c53a3cea390e5800bfdd0312
avincent2 <avincent2@slb.com>
2022-02-01 11:07:59 +0100

    add doctests to models to detect regressions on union types

--- app/model/model_curated.py
+++ app/model/model_curated.py
@@ -976,22 +976,33 @@ class namedProperty(DDMSBaseModel):
     """
     namedProperty Model
 
+    value accepts a float
+
     >>> from pprint import pprint
-    >>> original_data = {
-    ...     "associations": None,
-    ...     "description": "descr",
-    ...     "format": None,
-    ...     "name": "named property",
+    >>> named_property = namedProperty(**{
     ...     "value": 42.0
-    ... }
-    >>> named_property = namedProperty(**original_data)
+    ... })
     >>> pprint(named_property.dict())
     {'associations': None,
-     'description': 'descr',
+     'description': None,
      'format': None,
-     'name': 'named property',
+     'name': None,
      'unitKey': None,
      'value': 42.0,
+     'values': None}
+
+    value also accepts a string
+
+    >>> named_property = namedProperty(**{
+    ...     "value": "Lorem Ipsum"
+    ... })
+    >>> pprint(named_property.dict())
+    {'associations': None,
+     'description': None,
+     'format': None,
+     'name': None,
+     'unitKey': None,
+     'value': 'Lorem Ipsum',
      'values': None}
 
     """
@@ -1210,6 +1221,75 @@ class SpatialFilter(DDMSBaseModel):
 
 
 class geometryItem(DDMSBaseModel):
+    """
+    geometryItem Model
+
+    >>> from pprint import pprint
+
+    geometries field accepts various geojson geometries
+
+    >>> geometry_item = geometryItem(**{
+    ...    "geometries": [{
+    ...        "coordinates": [23.6, 34.65],
+    ...        "type": 'Point'
+    ...    }, {
+    ...        "coordinates": [[1.0, 2.3], [23.6, 34.65]],
+    ...        "type": 'MultiPoint'
+    ...    }, {
+    ...        "coordinates": [[1.0, 2.3], [23.6, 34.65]],
+    ...        "type": 'LineString'
+    ...    }, {
+    ...        "coordinates": [
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],
+    ...        "type": 'MultiLineString'
+    ...    }, {
+    ...        "coordinates": [
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],
+    ...        "type": 'Polygon'
+    ...    }, {
+    ...        "coordinates": [[
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],[
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ]],
+    ...        "type": 'MultiPolygon'
+    ...    }],
+    ...    "type": 'GeometryCollection',
+    ... })
+    >>> pprint(geometry_item.dict())
+    {'bbox': None,
+     'geometries': [{'bbox': None,
+                     'coordinates': [23.6, 34.65],
+                     'type': <Type_7.Point: 'Point'>},
+                    {'bbox': None,
+                     'coordinates': [[1.0, 2.3], [23.6, 34.65]],
+                     'type': <Type_5.MultiPoint: 'MultiPoint'>},
+                    {'bbox': None,
+                     'coordinates': [[1.0, 2.3], [23.6, 34.65]],
+                     'type': <Type_3.LineString: 'LineString'>},
+                    {'bbox': None,
+                     'coordinates': [[[1.0, 2.3], [23.6, 34.65]],
+                                     [[5.678, 12.3], [223.4, 34.2]]],
+                     'type': <Type_4.MultiLineString: 'MultiLineString'>},
+                    {'bbox': None,
+                     'coordinates': [[[1.0, 2.3], [23.6, 34.65]],
+                                     [[5.678, 12.3], [223.4, 34.2]]],
+                     'type': <Type_8.Polygon: 'Polygon'>},
+                    {'bbox': None,
+                     'coordinates': [[[[1.0, 2.3], [23.6, 34.65]],
+                                      [[5.678, 12.3], [223.4, 34.2]]],
+                                     [[[1.0, 2.3], [23.6, 34.65]],
+                                      [[5.678, 12.3], [223.4, 34.2]]]],
+                     'type': <Type_6.MultiPolygon: 'MultiPolygon'>}],
+     'type': <Type.GeometryCollection: 'GeometryCollection'>}
+
+    """
     bbox: Optional[List[float]] = None
     geometries: List[Union[
         GeoJsonPoint,
@@ -1223,6 +1303,153 @@ class geometryItem(DDMSBaseModel):
 
 
 class GeoJsonFeature(DDMSBaseModel):
+    """
+    GeoJsonFeature Model
+
+    >>> from pprint import pprint
+
+    geometry field accepts various geojson geometries
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [23.6, 34.65],
+    ...        "type": 'Point'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [23.6, 34.65],
+                  'type': <Type_7.Point: 'Point'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [[1.0, 2.3], [23.6, 34.65]],
+    ...        "type": 'MultiPoint'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [[1.0, 2.3], [23.6, 34.65]],
+                  'type': <Type_5.MultiPoint: 'MultiPoint'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [[1.0, 2.3], [23.6, 34.65]],
+    ...        "type": 'LineString'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [[1.0, 2.3], [23.6, 34.65]],
+                  'type': <Type_3.LineString: 'LineString'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],
+    ...        "type": 'MultiLineString'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [[[1.0, 2.3], [23.6, 34.65]],
+                                  [[5.678, 12.3], [223.4, 34.2]]],
+                  'type': <Type_4.MultiLineString: 'MultiLineString'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],
+    ...        "type": 'Polygon'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [[[1.0, 2.3], [23.6, 34.65]],
+                                  [[5.678, 12.3], [223.4, 34.2]]],
+                  'type': <Type_8.Polygon: 'Polygon'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry":  {
+    ...        "coordinates": [[
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ],[
+    ...             [[1.0, 2.3], [23.6, 34.65]],
+    ...             [[5.678, 12.3], [223.4, 34.2]]
+    ...        ]],
+    ...        "type": 'MultiPolygon'
+    ...    },
+    ...    "properties": {},
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [[[[1.0, 2.3], [23.6, 34.65]],
+                                   [[5.678, 12.3], [223.4, 34.2]]],
+                                  [[[1.0, 2.3], [23.6, 34.65]],
+                                   [[5.678, 12.3], [223.4, 34.2]]]],
+                  'type': <Type_6.MultiPolygon: 'MultiPolygon'>},
+     'properties': {},
+     'type': <Type_1.Feature: 'Feature'>}
+
+    properties accepts a dict with string keys and any kind of value
+
+    >>> geojson_feature = GeoJsonFeature(**{
+    ...    "geometry": {
+    ...        "coordinates": [23.6, 34.65],
+    ...        "type": 'Point'
+    ...    },
+    ...    "properties": {
+    ...        "prop_key_1": [1, "toto", 3, 4, {"some_dict_key": "some_dict_value"}]
+    ...    },
+    ...    "type": 'Feature',
+    ... })
+    >>> pprint(geojson_feature.dict())
+    {'bbox': None,
+     'geometry': {'bbox': None,
+                  'coordinates': [23.6, 34.65],
+                  'type': <Type_7.Point: 'Point'>},
+     'properties': {'prop_key_1': [1,
+                                   'toto',
+                                   3,
+                                   4,
+                                   {'some_dict_key': 'some_dict_value'}]},
+     'type': <Type_1.Feature: 'Feature'>}
+    """
     bbox: Optional[List[float]] = None
     geometry: Union[
         GeoJsonPoint,
