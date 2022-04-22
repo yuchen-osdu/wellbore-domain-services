d9a92f8d22f5b2142485ca9f9fefac72f7eaeed5
cmonmouton <cmonmouton@slb.com>
2022-04-22 19:03:51 +0200

    add test cases

--- tests/unit/statistics/statistics_routes_test.py
+++ tests/unit/statistics/statistics_routes_test.py
@@ -1,17 +1,31 @@
+import asyncio
 import time
-
 import pytest
 
-from tests.unit.routers.chunking_test import _create_chunks, _create_record, _create_df_from_response
-from app.bulk_persistence.dask.errors import BulkRecordNotFound
+from unittest import mock
+from app.bulk_persistence.statistics.bulk_statistics import BulkStatistics
+import osdu.core.api.storage.exceptions as osdu_storage_exception
+
+from tests.unit.generate_data import generate_df
+from tests.unit.routers.chunking_test import _create_chunks, _create_record, _create_df_from_response, Definitions
 
 
-def test_invalid_cases(app_configurable_with_testclient,mock_storage_client_holding_data, welllog_v3_record_list):
-    storage_client = mock_storage_client_holding_data(welllog_v3_record_list)
-    _, client = app_configurable_with_testclient(
-        storage_client_mock=storage_client,
-        fake_data_partition_id=True,
-    )
+def post_welllog_data(client, record_id, columns, range_index):
+    headers = {'content-type': 'application/x-parquet'}
+    chunking_url = Definitions['WellLog']["chunking_url"]
+
+    data_to_send = generate_df(columns, range_index).to_parquet(engine='pyarrow')
+
+    write_response = client.post(f'{chunking_url}/{record_id}/data', data=data_to_send, headers=headers)
+    assert write_response.status_code == 200
+
+
+def test_invalid_cases(app_configurable_with_testclient):
+
+    _, client = app_configurable_with_testclient(fake_data_partition_id=True)
+    valid_record_id = _create_record(client, "WellLog")
+
+    #todo: check response error message
 
     incorrect_record_id = 'incorrect-id'
     get_stats_response = client.get(f'/ddms/v3/welllogs/{incorrect_record_id}/data/statistics')
@@ -20,17 +34,44 @@ def test_invalid_cases(app_configurable_with_testclient,mock_storage_client_hold
     post_stats_response = client.post(f'/ddms/v3/welllogs/{incorrect_record_id}/data/statistics')
     assert post_stats_response.status_code == 404
 
-    valid_record_id = welllog_v3_record_list[0].id
-    get_valid_stats_response = client.get(f'/ddms/v3/welllogs/{valid_record_id}/data/statistics')
-    assert get_valid_stats_response.status_code == 404
+    valid_record_no_bulk_response = client.get(f'/ddms/v3/welllogs/{valid_record_id}/data/statistics')
+    assert valid_record_no_bulk_response.status_code == 404
 
     version = '123456789'
-    invalid_version_id_get_stats_response = client.get(
+    valid_record_invalid_version_response = client.get(
         f"/ddms/v3/welllogs/{valid_record_id}/versions/{version}/data/statistics")
-    assert invalid_version_id_get_stats_response.status_code == 404
+    assert valid_record_invalid_version_response.status_code == 404
 
 
-def test_compute_stats(app_configurable_with_testclient, mock_storage_client_holding_data, welllog_v3_record_list):
+def test_with_bulk_no_stats(app_configurable_with_testclient):
+
+    with mock.patch.object(BulkStatistics, '_fetch_statistics_meta_file') as bob:
+        bob.side_effect = osdu_storage_exception.ResourceNotFoundException()
+
+        _, client = app_configurable_with_testclient(fake_data_partition_id=True, disable_bulk_consistency=True)
+        valid_record_id = _create_record(client, "WellLog")
+        post_welllog_data(client, valid_record_id, ['int-A'], range(10))
+
+        valid_record_with_bulk_response = client.get(f'/ddms/v3/welllogs/{valid_record_id}/data/statistics')
+        assert valid_record_with_bulk_response.status_code == 404
+        assert valid_record_with_bulk_response.content == b'{"detail":"Statistics do not exist"}'
+
+
+def test_with_bulk_stats_not_complete(app_configurable_with_testclient):
+
+    with mock.patch.object(BulkStatistics, '_fetch_statistics_meta_file') as bob:
+        bob.return_value = {"computation_status": "started"}
+
+        _, client = app_configurable_with_testclient(fake_data_partition_id=True, disable_bulk_consistency=True)
+        valid_record_id = _create_record(client, "WellLog")
+        post_welllog_data(client, valid_record_id, ['int-A'], range(10))
+
+        valid_record_with_bulk_response = client.get(f'/ddms/v3/welllogs/{valid_record_id}/data/statistics')
+        assert valid_record_with_bulk_response.status_code == 404
+        assert valid_record_with_bulk_response.content == b'{"detail":"Statistics computation not finished yet"}'
+
+
+def test_compute_stats(app_configurable_with_testclient):
 
     _, client = app_configurable_with_testclient(fake_data_partition_id=True)
     record_id = _create_record(client, "WellLog")
@@ -38,11 +79,11 @@ def test_compute_stats(app_configurable_with_testclient, mock_storage_client_hol
                                                                         (['MD', 'X'], range(10, 30)),
                                                                         (['MD', 'X'], range(25, 40))])
 
-    compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
-    assert compute_stats_response.status_code == 200
+    # compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
+    # assert compute_stats_response.status_code == 200
 
     # todo: it should be replaced by a more robust mechanism that folder existence check in BulkStatistics class
-    time.sleep(.5)
+    # time.sleep(.5)
 
     compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
     assert compute_stats_response.status_code == 409
@@ -56,19 +97,19 @@ def test_get_stats(app_configurable_with_testclient):
                                                                         (['MD', 'X'], range(10, 30)),
                                                                         (['MD', 'X'], range(25, 40))])
 
-    compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
-    assert compute_stats_response.status_code == 200
+    # compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
+    # assert compute_stats_response.status_code == 200
     # todo: it should be replaced by a more robust mechanism that folder existence check in BulkStatistics class
-    time.sleep(1)
+    # time.sleep(1)
 
     record_response = client.get(f'/ddms/v3/welllogs/{record_id}')
     assert record_response.status_code == 200
-    bob = record_response.json()
+    record_json = record_response.json()
 
     get_stats_response = client.get(f'/ddms/v3/welllogs/{record_id}/data/statistics')
     assert get_stats_response.status_code == 200
 
-    version = bob['version']
+    version = record_json['version']
     get_stats_version_response = client.get(f"/ddms/v3/welllogs/{record_id}/versions/{version}/data/statistics")
     assert get_stats_version_response.status_code == 200
     df_result_1 = _create_df_from_response(get_stats_version_response)
@@ -101,8 +142,8 @@ def test_get_stats_from_not_computable_columns(app_configurable_with_testclient)
                        ['int-A', 'string-B', 'bool-C', 'string-D'],
                        range(20))])
 
-    compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
-    assert compute_stats_response.status_code == 200
+    # compute_stats_response = client.post(f'/ddms/v3/welllogs/{record_id}/data/statistics')
+    # assert compute_stats_response.status_code == 200
     # todo: it should be replaced by a more robust mechanism that folder existence check in BulkStatistics class
     time.sleep(1)
 
@@ -120,3 +161,37 @@ def test_get_stats_from_not_computable_columns(app_configurable_with_testclient)
     # todo: Update BulkStatistics class + swagger when only not computable columns are requested, 400 error is expected
     get_stats_response_2 = client.get(f'/ddms/v3/welllogs/{record_id}/data/statistics', params=params)
     assert get_stats_response_2.status_code == 400
+
+
+# @pytest.fixture()
+# async def bob_bob():
+#     tenant = mock.Mock()
+#     tenant.project_id = ''
+#     tenant.bucket_name = ""
+#
+#     with mock.patch('app.bulk_persistence.statistics.bulk_statistics.resolve_tenant', mock.AsyncMock(return_value=tenant)) as bobby:
+#         print(bobby)
+#
+#         # from app.bulk_persistence import resolve_tenant
+#         # toto = await resolve_tenant('bob')
+#         yield
+
+
+@pytest.mark.asyncio
+async def test_get_stats_after_post_data(app_configurable_with_testclient):
+
+    _, client = app_configurable_with_testclient(fake_data_partition_id=True,
+                                                 disable_bulk_consistency=True)
+    record_id = _create_record(client, "WellLog")
+    post_welllog_data(client, record_id, ['int-A', 'string-B', 'bool-C', 'string-D'], range(100))
+
+    from datetime import datetime
+    print("post bulk - AFTER", datetime.now())
+    # todo: it should be replaced by a more robust mechanism that folder existence check in BulkStatistics class
+    # time.sleep(30)
+    await asyncio.sleep(5)
+
+    print("get stats - BEFORE", datetime.now(), flush=True)
+    get_stats_response = client.get(f'/ddms/v3/welllogs/{record_id}/data/statistics')
+    assert get_stats_response.status_code == 200, get_stats_response.content
+    print("get stats - AFTER", datetime.now(), flush=True)
