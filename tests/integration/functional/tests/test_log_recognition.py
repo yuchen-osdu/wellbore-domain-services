68ecedff50ad61df958316e744c4b849f176f1a4
yvernet@slb.com <yvernet@slb.com>
2022-06-08 10:51:23 +0200

    adding a simple integration test on log recognition

---
+++ tests/integration/functional/tests/test_log_recognition.py
@@ -0,0 +1,19 @@
+import pytest
+
+from wdms_client.request_builders import make_base_request_proto
+from wdms_client.request_runner import RequestRunner
+
+
+@pytest.mark.tag('smoke', 'log_recognition')
+def test_basic_log_recognition(with_wdms_env):
+    result = RequestRunner(make_base_request_proto('POST', '/log-recognition/family', payload={
+        "label": "GRD",
+        "log_unit": "GAPI",
+        "description": "LDTD Gamma Ray"
+    })).call(with_wdms_env)
+    result.assert_ok()
+
+    resobj = result.get_response_obj()
+    assert resobj["family"] == "Gamma Ray"
+    assert resobj["log_unit"] == "GAPI"
+    assert resobj["base_unit"] == "gAPI"
