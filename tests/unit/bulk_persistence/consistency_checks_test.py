aa31d88ea0004d80e795b9e82ab4813e5ebdae60
Victor Dosda <vdosda@slb.com>
2022-06-08 11:07:10 +0200

    adapted tests welllog check nunber of column consistency

--- tests/unit/bulk_persistence/consistency_checks_test.py
+++ tests/unit/bulk_persistence/consistency_checks_test.py
@@ -8,13 +8,13 @@ from app.bulk_persistence.consistency_checks import DataConsistencyChecks
 
 
 def test_get_data_columns_name():
-    computed = DataConsistencyChecks._get_data_columns_name(
+    computed = DataConsistencyChecks._get_name_and_count_data_columns(
         ["GR[1]", "GR[2]", "DEN[1324]", "VSHALE[1324]", "", "A[1324][456]"]
     )
 
-    assert not DeepDiff(computed, {"GR", "DEN", "VSHALE", "A[1324]"})
+    assert not DeepDiff(computed, {"GR": 2, "DEN": 1, "VSHALE": 1, "A[1324]": 1})
 
-    assert DataConsistencyChecks._get_data_columns_name([]) == set()
-    assert DataConsistencyChecks._get_data_columns_name([""]) == set()
-    assert DataConsistencyChecks._get_data_columns_name(["[foo]"]) == {"[foo]"}
-    assert DataConsistencyChecks._get_data_columns_name(["[1234]"]) == {"[1234]"}
+    assert DataConsistencyChecks._get_name_and_count_data_columns([]) == {}
+    assert DataConsistencyChecks._get_name_and_count_data_columns([""]) == {}
+    assert DataConsistencyChecks._get_name_and_count_data_columns(["[foo]"]) == {"[foo]": 1}
+    assert DataConsistencyChecks._get_name_and_count_data_columns(["[1234]"]) == {"[1234]": 1}
