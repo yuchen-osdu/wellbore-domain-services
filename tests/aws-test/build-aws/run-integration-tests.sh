7743008983ab8235911301a381557c1d2995c1e6
Elsi Abraham <elsi.abraham@parivedasolutions.com>
2022-06-10 21:50:24 +0000

    Update run-integration-tests.sh

--- tests/aws-test/build-aws/run-integration-tests.sh
+++ tests/aws-test/build-aws/run-integration-tests.sh
@@ -42,7 +42,8 @@ cd tests/integration
 
 python3 gen_postman_env.py --token $svctoken --base_url $svc_url --cloud_provider "aws" --acl_domain $acl_domain --legal_tag $legal_tag --data_partition $tenant
 
-pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=!search || TEST_EXIT_CODE=$?
+pytest ./functional --environment="./generated/postman_environment.json" --filter-tag=!search 
+TEST_EXIT_CODE=$?
 
 echo Delete legaltag after Integration Tests...
 curl --location --request DELETE "$LEGAL_URL"'legaltags/opendes-wellddmstestlegaltag' \
@@ -50,7 +51,6 @@ curl --location --request DELETE "$LEGAL_URL"'legaltags/opendes-wellddmstestlega
 --header 'data-partition-id: opendes' \
 --header 'Content-Type: application/json'
 
-#deactivate
-echo $TEST_EXIT_CODE
+deactivate
 
 exit $TEST_EXIT_CODE
