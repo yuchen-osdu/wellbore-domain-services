f1493cec4cbd797e3764e05b9d16501a3ee49063
DIR\FSerin <fserin@slb.com>
2023-02-08 15:36:58 +0100

    Enabling describe with the worker

---
+++ tests/integration/functional/tests/test_describe.py
@@ -0,0 +1,301 @@
+# Copyright 2021 Schlumberger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from .test_chunking import ParquetSerializer, JsonSerializer, create_record, build_request_post_data, \
+    build_request, build_base_url_without_dask, create_session, \
+    build_request_post_chunk, complete_session
+from ..generate_dataframe import generate_df
+
+from wdms_client.request_runner import RequestRunner
+from .fixtures import with_wdms_env
+
+
+def build_request_get_data_describe(entity_type: str, record_id: str, filters=None) -> RequestRunner:
+    url = build_base_url_without_dask(entity_type) + f'/{record_id}/data'
+    url = url + '?' + 'describe=true'
+    if filters:
+        url = url + '?' + '&'.join(f'{k}={v}' for k, v in filters.items())
+
+    return build_request(f'{entity_type} get data', 'GET', url)
+
+
+@pytest.mark.tag('describe', 'smoke')
+@pytest.mark.parametrize('entity_type', ["well_log", "wellbore_trajectory"])
+@pytest.mark.parametrize('serializer', [ParquetSerializer(), JsonSerializer()])
+def test_describe_one_chunk_without_session(with_wdms_env, entity_type, serializer):
+    col_and_nb_col = {'MD': 1, 'X': 1}
+    with create_record(with_wdms_env, entity_type, col_and_nb_col) as record_id:
+        data = generate_df(col_and_nb_col.keys(), range(8))
+        data_to_send = serializer.dump(data)
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        build_request_post_data(entity_type, record_id, data_to_send).call(with_wdms_env, headers=headers).assert_ok()
+
+        result = build_request_get_data_describe(entity_type, record_id).call(with_wdms_env, headers=headers,
+                                                                              assert_status=200)
+        res_as_dict = result.response.json()
+        assert res_as_dict["numberOfRows"] == 8
+        assert res_as_dict["columns"] == ["MD", "X"]
+
+
+@pytest.mark.tag('describe', 'smoke')
+@pytest.mark.parametrize('entity_type', ["well_log", "wellbore_trajectory"])
+@pytest.mark.parametrize('serializer', [ParquetSerializer(), JsonSerializer()])
+def test_describe_one_chunk_with_session_commit(with_wdms_env, entity_type, serializer):
+
+    col_and_nb_col = {'MD': 1, 'X': 1}
+    with create_record(with_wdms_env, entity_type, col_and_nb_col) as record_id:
+        expected = generate_df(col_and_nb_col.keys(), range(8))
+
+        # create session
+        session_id = create_session(with_wdms_env, entity_type, record_id, True)  # mode overwrite
+
+        # post chunk
+        build_request_post_chunk(
+            entity_type, record_id, session_id, serializer.dump(expected)
+        ).call(
+            with_wdms_env, headers={'Content-Type': serializer.mime_type},
+        ).assert_ok()
+
+        # commit session
+        complete_session(with_wdms_env, entity_type, record_id, session_id, True)  # commit
+
+        # then check describe
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+        result = build_request_get_data_describe(entity_type, record_id).call(with_wdms_env, headers=headers,
+                                                                              assert_status=200)
+        res_as_dict = result.response.json()
+        assert res_as_dict["numberOfRows"] == 8
+        assert res_as_dict["columns"] == ["MD", "X"]
+
+
+@pytest.mark.tag('describe', 'smoke')
+def test_describe_multiple_chunks_with_session_commit(with_wdms_env):
+    # well log on parquet
+    entity_type = "well_log"
+    serializer = ParquetSerializer()
+    col_and_nb_col = {'MD': 1, 'X': 1, 'Y': 1, 'Z': 1}
+    with create_record(with_wdms_env, entity_type, col_and_nb_col) as record_id:
+        data = generate_df(col_and_nb_col.keys(), range(1000))
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        # create session
+        session_id = create_session(with_wdms_env, entity_type, record_id, True)  # mode overwrite
+
+        # post a chunk
+        chunks = [data.iloc[idx:idx + 100].to_parquet(engine="pyarrow") for idx in range(0, 1000, 100)]
+
+        for chunk in chunks:
+            build_request_post_chunk(
+                entity_type, record_id, session_id, chunk
+            ).call(
+                with_wdms_env, headers=headers,
+            ).assert_ok()
+
+        # commit session
+        complete_session(with_wdms_env, entity_type, record_id, session_id, True)
+
+        # then check describe
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+        result = build_request_get_data_describe(entity_type, record_id).call(with_wdms_env, headers=headers,
+                                                                              assert_status=200)
+        res_as_dict = result.response.json()
+        assert res_as_dict["numberOfRows"] == 1000
+        assert res_as_dict["columns"] == ["MD", "X", "Y", "Z"]
+
+
+@pytest.mark.tag('chunking', 'smoke')
+def test_describe_with_offset_filter_without_session(with_wdms_env):
+    # well log on parquet
+    entity_type = "well_log"
+    serializer = ParquetSerializer()
+    col_and_nb_col = {'MD': 1, 'X': 1}
+    with create_record(with_wdms_env, entity_type,col_and_nb_col) as record_id:
+        size = 100
+        data = generate_df(col_and_nb_col.keys(), range(size))
+        data_to_send = serializer.dump(data)
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        # post data
+        build_request_post_data(entity_type, record_id, data_to_send).call(with_wdms_env, headers=headers).assert_ok()
+
+        validation_list = [  # tuple (params, expected_status, expected data)
+            ({"offset": 0}, 200, data),
+            ({"offset": "0"}, 200, data),
+            ({"offset": 1}, 200, data.tail(size - 1)),
+            ({"offset": int(size / 2)}, 200, data.tail(int(size / 2))),
+            ({"offset": size - 1}, 200, data.tail(1)),
+            ({"offset": 1, "curves": "X"}, 200, data[['X']].tail(size - 1)),
+            # if offset >= number of rows, returns an empty dataFrame with columns
+            ({"offset": size}, 200, data.tail(0)),
+            ({"offset": size + 50}, 200, data.tail(0)),
+            # invalid offset
+            ({"offset": -2}, 422, None),
+            ({"offset": "false"}, 422, None)
+        ]
+
+        for (params, expected_status, expected_data) in validation_list:
+            headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+            result = build_request_get_data_describe(entity_type, record_id
+                                                     ).call(with_wdms_env,
+                                                            params=params,
+                                                            headers=headers,
+                                                            assert_status=expected_status)
+            if result.ok:
+                res_as_dict = result.response.json()
+                assert res_as_dict["numberOfRows"] == len(expected_data.index)
+                assert res_as_dict["columns"] == expected_data.columns.tolist()
+
+
+@pytest.mark.tag('chunking', 'smoke')
+def test_describe_with_offset_filter_with_session(with_wdms_env):
+    # well log on parquet
+    entity_type = "well_log"
+    serializer = ParquetSerializer()
+    col_and_nb_col = {'MD': 1, 'X': 1}
+    with create_record(with_wdms_env, entity_type,col_and_nb_col) as record_id:
+        size = 100
+        data = generate_df(col_and_nb_col.keys(), range(size))
+        data_to_send = serializer.dump(data)
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        # create session
+        session_id = create_session(with_wdms_env, entity_type, record_id, True)  # mode overwrite
+
+        # post data
+        build_request_post_chunk(
+            entity_type, record_id, session_id, data_to_send
+        ).call(
+            with_wdms_env, headers={'Content-Type': serializer.mime_type},
+        ).assert_ok()
+
+        # commit session
+        complete_session(with_wdms_env, entity_type, record_id, session_id, True)  # commit
+
+        validation_list = [  # tuple (params, expected_status, expected data)
+            ({"offset": 0}, 200, data),
+            ({"offset": "0"}, 200, data),
+            ({"offset": 1}, 200, data.tail(size - 1)),
+            ({"offset": int(size / 2)}, 200, data.tail(int(size / 2))),
+            ({"offset": size - 1}, 200, data.tail(1)),
+            ({"offset": 1, "curves": "X"}, 200, data[['X']].tail(size - 1)),
+            # if offset >= number of rows, returns an empty dataFrame with columns
+            ({"offset": size}, 200, data.tail(0)),
+            ({"offset": size + 50}, 200, data.tail(0)),
+            # invalid offset
+            ({"offset": -2}, 422, None),
+            ({"offset": "false"}, 422, None)
+        ]
+
+        for (params, expected_status, expected_data) in validation_list:
+            headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+            result = build_request_get_data_describe(entity_type, record_id
+                                                     ).call(with_wdms_env,
+                                                            params=params,
+                                                            headers=headers,
+                                                            assert_status=expected_status)
+            if result.ok:
+                res_as_dict = result.response.json()
+                assert res_as_dict["numberOfRows"] == len(expected_data.index)
+                assert res_as_dict["columns"] == expected_data.columns.tolist()
+
+
+@pytest.mark.tag('describe', 'smoke')
+def test_describe_with_column_filter_without_session(with_wdms_env):
+    # well log on parquet
+    entity_type = "well_log"
+    serializer = ParquetSerializer()
+    col_and_nb_col = {'MD': 1, 'X': 1, 'Y': 1, 'Z': 1, '2D': 3}
+    with create_record(with_wdms_env, entity_type, col_and_nb_col) as record_id:
+        size = 100
+        data = generate_df(['MD', 'X', 'Y', 'Z', '2D[0]', '2D[1]', '2D[2]'], range(size))
+        data_to_send = serializer.dump(data)
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        # post data
+        build_request_post_data(entity_type, record_id, data_to_send).call(with_wdms_env, headers=headers).assert_ok()
+
+        validation_list = [  # tuple (params, expected_status, expected data)
+            ({"curves": "MD"}, 200, data[['MD']]),
+            ({"curves": "X, Y, Z"}, 200, data[['X', 'Y', 'Z']]),
+            ({"curves": "W, X"}, 404, data[['X']]),
+            ({"curves": "2D[0]"}, 200, data[['2D[0]']]),
+            ({"curves": "2D[0:1]"}, 200, data[['2D[0]', '2D[1]']]),
+            ({"curves": "2D"}, 200, data[['2D[0]', '2D[1]', '2D[2]']]),
+            ({"curves": "Y, X"}, 200, data[['Y', 'X']]),  # filter order should be maintain
+            ({"curves": "2D[1], 2D[0]"}, 200, data[['2D[1]', '2D[0]']]),  # filter order should be maintain
+        ]
+
+        for (params, expected_status, expected_data) in validation_list:
+            headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+            result = build_request_get_data_describe(entity_type, record_id
+                                                     ).call(with_wdms_env,
+                                                            params=params,
+                                                            headers=headers,
+                                                            assert_status=expected_status)
+            if result.ok:
+                res_as_dict = result.response.json()
+                assert res_as_dict["numberOfRows"] == len(expected_data.index)
+                assert res_as_dict["columns"] == expected_data.columns.tolist()
+
+
+@pytest.mark.tag('describe', 'smoke')
+def test_describe_with_column_filter_with_session(with_wdms_env):
+    # well log on parquet
+    entity_type = "well_log"
+    serializer = ParquetSerializer()
+    col_and_nb_col = {'MD': 1, 'X': 1, 'Y': 1, 'Z': 1, '2D': 3}
+    with create_record(with_wdms_env, entity_type, col_and_nb_col) as record_id:
+        size = 100
+        data = generate_df(['MD', 'X', 'Y', 'Z', '2D[0]', '2D[1]', '2D[2]'], range(size))
+        data_to_send = serializer.dump(data)
+        headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+
+        # create session
+        session_id = create_session(with_wdms_env, entity_type, record_id, True)  # mode overwrite
+
+        # post data
+        build_request_post_chunk(
+            entity_type, record_id, session_id, data_to_send
+        ).call(
+            with_wdms_env, headers={'Content-Type': serializer.mime_type},
+        ).assert_ok()
+
+        # commit session
+        complete_session(with_wdms_env, entity_type, record_id, session_id, True)  # commit
+
+        validation_list = [  # tuple (params, expected_status, expected data)
+            ({"curves": "MD"}, 200, data[['MD']]),
+            ({"curves": "X, Y, Z"}, 200, data[['X', 'Y', 'Z']]),
+            ({"curves": "W, X"}, 404, data[['X']]),
+            ({"curves": "2D[0]"}, 200, data[['2D[0]']]),
+            ({"curves": "2D[0:1]"}, 200, data[['2D[0]', '2D[1]']]),
+            ({"curves": "2D"}, 200, data[['2D[0]', '2D[1]', '2D[2]']]),
+            ({"curves": "Y, X"}, 200, data[['Y', 'X']]),  # filter order should be maintain
+            ({"curves": "2D[1], 2D[0]"}, 200, data[['2D[1]', '2D[0]']]),  # filter order should be maintain
+        ]
+
+        for (params, expected_status, expected_data) in validation_list:
+            headers = {'Content-Type': serializer.mime_type, 'Accept': serializer.mime_type}
+            result = build_request_get_data_describe(entity_type, record_id
+                                                     ).call(with_wdms_env,
+                                                            params=params,
+                                                            headers=headers,
+                                                            assert_status=expected_status)
+            if result.ok:
+                res_as_dict = result.response.json()
+                assert res_as_dict["numberOfRows"] == len(expected_data.index)
+                assert res_as_dict["columns"] == expected_data.columns.tolist()
